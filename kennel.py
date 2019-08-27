40f967cd2d0fe6ac45cff3021005373d553c789b
Mary West <westmarye1219@gmail.com>
2019-08-27 10:40:11 -0500

    exercise complete

---
+++ kennel.py
@@ -0,0 +1,49 @@
+animals_in_kennel = [
+    {
+        "id": 1,
+        "breed": "German Shepherd",
+        "age": 3,
+        "name": "Jack"
+    },
+    {
+        "id": 2,
+        "breed": "Siamese",
+        "age": 9,
+        "name": "Shy"
+    },
+    {
+        "id": 3,
+        "breed": "Labradoodle",
+        "age": 5,
+        "name": "Avett"
+    },
+    {
+        "id": 4,
+        "breed": "Shnauzer",
+        "age": 1,
+        "name": "Gypsy"
+    },
+]
+
+for x in animals_in_kennel:
+  if len(x)==4:
+    print(x)
+
+
+# Key "id" = 1
+# Key "breed" = German Shepherd
+# Key "age" = 3
+# Key "name" = Jack
+# Key "id" = 2
+# Key "breed" = Siamese
+# Key "age" = 9
+# Key "name" = Shy
+# Key "id" = 3
+# Key "breed" = Labradoodle
+# Key "age" = 5
+# Key "name" = Avett
+# Key "id" = 4
+# Key "breed" = Shnauzer
+# Key "age" = 1
+# Key "name" = Gypsy
+
